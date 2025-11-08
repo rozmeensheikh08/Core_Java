26d7d13a5a91378d2d42fd61dc686a674fe5df95
Rozmeen Sheikh <rozmeensheikh5@gmail.com>
2025-11-08 13:22:48 +0530

    Password Checker

--- src/MiniProjects/PasswordChecker.java
+++ src/MiniProjects/PasswordChecker.java
@@ -2,20 +2,14 @@ package MiniProjects;
 
 import java.util.Scanner;
 
-//Conditions
-//Password must be at least 8 characters
-//Password must contain at least one number
-//Password must contain at least one Upper case letter
-//Password must contain at least one Lower case letter
-
 public class PasswordChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print(" Enter Password : ");
         String pass = sc.nextLine();
         String[] nums = {"0","1","2","3","4","5","6","7","8","9"};
-       String lowerPass = pass.toLowerCase();
-       String upperPass = pass.toUpperCase();
+        String lowerPass = pass.toLowerCase();
+        String upperPass = pass.toUpperCase();
         int count =  0 ;
 
         if (pass.length()>=8){
@@ -44,8 +38,7 @@ public class PasswordChecker {
         }
 
         sc.close();
+
     }
 }
-
-
 
